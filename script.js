bece18d467ebd0845c210da628f585f8188d1130
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-12-27 23:22:23 +0200

    result

--- script.js
+++ script.js
@@ -7,12 +7,29 @@ const userAnswer = prompt("Rock, scissors or paper?"); // NULL если юзер
 
 const randomIndex = getRandomNumber(0, 2);
 const computerAnswer = tools[randomIndex];
-// console.log(computerAnswer);
+
+console.log(`Your answer is ${userAnswer}`);
+console.log(`Computer answer is ${computerAnswer}`);
 
 const startGame = (userAnswer, computerAnswer) => {
-    if(!userAnswer) {
+    userAnswer = userAnswer.toLowerCase();
+    if (!userAnswer || !tools.includes(userAnswer)) {
         return "Wrong answer"; // если не введено в окно ничего 
     }     
+
+    let res = ''
+
+    const isDraw = userAnswer === computerAnswer;
+    const isUserWin = (userAnswer === 'rock' && computerAnswer === 'scissors') || 
+    (userAnswer === 'paper' && computerAnswer === 'rock') || 
+    (userAnswer === 'scissors' && computerAnswer === 'paper');
+
+    if(isDraw) res = "It's draw";
+    else if (isUserWin) res = "You Win :)";
+    else res = "You lose :("
+
+    return res;
 };
+
 const result = startGame(userAnswer, computerAnswer);
 alert(result);
