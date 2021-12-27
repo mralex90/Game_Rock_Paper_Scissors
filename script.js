0b94a34b6e8661b72d028ef2649893c0a05e2c83
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-12-27 23:42:57 +0200

    комментарии

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const tools = ['rock', 'paper', 'scissors'];
 
 const getRandomNumber = (min, max) => 
-    Math.floor(Math.random() * (max - min + 1) + min);
+    Math.floor(Math.random() * (max - min + 1) + min); // ф-ия для генерации рандомного целого! числа
 
-const userAnswer = prompt("Rock, scissors or paper?"); // NULL если юзер не ввел в окно ничего
+const userAnswer = prompt("Rock, scissors or paper?"); // получение ответа от юзера / NULL если юзер не ввел в окно ничего
 
-const randomIndex = getRandomNumber(0, 2);
-const computerAnswer = tools[randomIndex];
+const randomIndex = getRandomNumber(0, 2); // будет возвращать целое рандомное число
+const computerAnswer = tools[randomIndex]; // tools будет подбирать индекс: 0(rock) / 1(paper) / 2(scissors)
 
 console.log(`Your answer is ${userAnswer}`);
 console.log(`Computer answer is ${computerAnswer}`);
@@ -14,10 +14,10 @@ console.log(`Computer answer is ${computerAnswer}`);
 const startGame = (userAnswer, computerAnswer) => {
     userAnswer = userAnswer.toLowerCase();
     if (!userAnswer || !tools.includes(userAnswer)) {
-        return "Wrong answer"; // если не введено в окно ничего 
+        return "Wrong answer"; // если ничего не введено в окно или введено не правильно
     }     
 
-    let res = ''
+    let res = '';
 
     const isDraw = userAnswer === computerAnswer;
     const isUserWin = (userAnswer === 'rock' && computerAnswer === 'scissors') || 
